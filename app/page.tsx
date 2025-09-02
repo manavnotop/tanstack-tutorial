0e1596689bc47f316d8dd5c55223e1b9a3504e34
manavnotop <manuagarwal1443@gmail.com>
2025-09-02 22:14:43 +0530

    complete basic tanstack tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client"
+
+import { getUsers } from "@/server/users";
+import { useQuery } from "@tanstack/react-query";
+
+interface User {
+  id: number,
+  name: string
+}
 
 export default function Home() {
+
+  const query = useQuery({ queryKey: ['todos'], queryFn: getUsers})
+  
   return (
-    <div>
-      hello world
+    <div className="flex items-center justify-center flex-col h-screen">
+      {query.data?.map((user: User) => (
+        <div key={user.id}> {user.name} </div>
+      ))}
     </div>
   );
 }
