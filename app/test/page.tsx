09fd602df46a0dc62aaeac715451131ded334742
manavnotop <manuagarwal1443@gmail.com>
2025-09-02 22:58:14 +0530

    Add: test page to test muscle memory

---
+++ app/test/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query";
+
+interface PostBody { 
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
+async function getPosts(): Promise<PostBody []>{
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json()
+}
+
+export default function Test(){
+
+  const query = useQuery({ queryKey: ['posts'], queryFn: getPosts})
+
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      {query.data?.map((post) => (
+        <div className="p-5 m-5" key={post.id}> {post.body} </div>
+      ))}
+    </div>
+  )
+}
